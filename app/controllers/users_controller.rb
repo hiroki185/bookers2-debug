7d59407345009e4de7004685954fa5ebad051182
hiroki185 <68848005+hiroki185@users.noreply.github.com>
2024-01-23 08:07:27 +0000

    残り33項目

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,11 +15,12 @@ class UsersController < ApplicationController
   end
 
   def edit
+   @user = current_user
   end
 
   def update
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      redirect_to user_path(@user), notice: "You have updated user successfully."
     else
       render "show"
     end
